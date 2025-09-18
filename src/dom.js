23398bf7da7446530e758d85aef7c398ccd5b576
Kingmouaad <kjhfu80@gmail.com>
2025-09-18 20:07:11 +0200

    fourth

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,6 @@
 import "./todo.css";
 import { Task } from "./task.js";
-import { project } from "./project.js";
+import { project, project } from "./project.js";
 
 Task;
 project;
@@ -8,10 +8,13 @@ project;
 const fileicon = document.querySelector("#addproject");
 const addproject = document.querySelector("#mdd");
 const cancelproject = document.querySelector("#addproject");
+const projectmenu = document.querySelector("#seconddiv");
 // fileicon.addEventListener("submit",()=>{
 //     const project =new Task()
-
 // })
-fileicon.addEventListener("submit", () => {
-  const project = new Task();
+addproject.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const projectt = new project();
+  const element = projectt.createdom();
+  projectmenu.appendChild();
 });
