7e4b3b1bdb22c326c1fc78132ddb5693572a0149
Kingmouaad <kjhfu80@gmail.com>
2025-09-19 17:23:40 +0200

    six

--- src/dom.js
+++ src/dom.js
@@ -4,19 +4,33 @@ import { project } from "./project.js";
 
 const fileicon = document.querySelector("#add-project");
 const addproject = document.querySelector("#mdd");
-const cancelproject = document.querySelector("#addproject");
+const cancelproject = document.querySelector("#cancel");
 const projectmenu = document.querySelector("#seconddiv");
 const formproject = document.querySelector("#form");
 const formcontainer = document.querySelector("#thirddiv");
+
 fileicon.addEventListener("click", () => {
   formcontainer.style.display = "block";
 });
-cancelproject.addEventListener("click", () => {
-  formcontainer.style.display = "none";
-});
-formproject.addEventListener("submit", (e) => {
+let i;
+if (localStorage) {
+  for (let j = 0; j <= i; j++) {
+    projectmenu.appendChild(JSON.parse(localStorage.getItem(`${j}`)));
+  }
+}
+addproject.addEventListener("click", (e) => {
   e.preventDefault();
-  const projectt = new project();
+  e.stopPropagation();
+  i++;
+
+  const projectname = document.querySelector("#project").value;
+  const projectcolor = document.querySelector("#color").value;
+  const projectt = new project(projectname, projectcolor);
   const element = projectt.createdom();
   projectmenu.appendChild(element);
+  localStorage.setItem(`${i}`, JSON.stringify(element.outerHTML));
+  formcontainer.style.display = "none";
+});
+cancelproject.addEventListener("click", () => {
+  formcontainer.style.display = "none";
 });
